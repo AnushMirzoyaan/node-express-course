01a9b72fede4ad591650e6deb08e9322b0db5ba1
AnushMirzoyaan <anush.mirzoyan1999@gmail.com>
2023-02-27 20:58:18 +0400

    globals and modules

---
+++ node-tutorial/2-globals.js
@@ -0,0 +1,5 @@
+console.log(__dirname)
+console.log(__filename)
+console.log(process)
+
+setInterval(() => console.log("hello") , 5000)
---
+++ node-tutorial/3-modules.js
@@ -0,0 +1,10 @@
+const susan = "Susan"
+const peter = "Peter"
+
+const sayHi = (name) => {
+    console.log(`Hello there ${name}`)
+}
+
+sayHi("John")
+sayHi(susan)
+sayHi(peter)
